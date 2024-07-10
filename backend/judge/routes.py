f71024f339557b96ce2be36bf2253fdd60c159e1
FathiHanana <hananaf2001@gmail.com>
2024-07-10 14:50:02 +0530

    css divided

--- backend/judge/routes.py
+++ backend/judge/routes.py
@@ -6,3 +6,7 @@ judge_bp = Blueprint('judge', __name__, template_folder="../../frontend/template
 @judge_bp.route('/')
 def judge_index():
     return render_template('index_judge.html')
+
+@judge_bp.route('/calendar')
+def judge_calendar():
+    return render_template('judge_calendar.html')
