a39cfad75d6541fd6c74a90f231636fcf7d99b2b
FathiHanana <hananaf2001@gmail.com>
2024-07-11 14:04:07 +0530

    database connected

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ from backend.user.routes import user_bp
 from backend.admin.routes import admin_bp
 from backend.lawyer.routes import lawyer_bp
 from backend.judge.routes import judge_bp
+from extensions import mysql
 
 app = Flask(__name__, template_folder="frontend/templates", static_folder="frontend/static")
 
 # Set the secret key for the flash message
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+# MySQL configurations
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'nihla'
+app.config['MYSQL_PASSWORD'] = 'EX6826679#'
+app.config['MYSQL_DB'] = 'districtcourt'
+
+# Initialize MySQL
+mysql.init_app(app)
 
 # Register the blueprints
 app.register_blueprint(user_bp, url_prefix='/user')
@@ -17,4 +27,3 @@ app.register_blueprint(judge_bp, url_prefix='/judge')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
--- backend/admin/routes.py
+++ backend/admin/routes.py
@@ -1,5 +1,5 @@
-
 from flask import Blueprint, render_template
+from extensions import mysql
 
 admin_bp = Blueprint('admin', __name__, template_folder="../../frontend/templates/admin")
 
@@ -26,3 +26,10 @@ def admin_addJudge():
 @admin_bp.route('/dashboard')
 def admin_dashboard():
     return render_template('admin_dashboard.html')
+
+@admin_bp.route('/test-db')
+def test_db():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT 1''')
+    results = cur.fetchall()
+    return f"DB Test Results: Success {results}"
